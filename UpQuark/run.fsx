aa0165e95cfdca112500d9cf4dd8030ddca5ac7b
David Roberts <david.roberts63@gmail.com>
2017-05-02 23:57:19 -0500

    Trying entity serialization

--- UpQuark/run.fsx
+++ UpQuark/run.fsx
@@ -5,6 +5,7 @@ open System.Linq
 open System.Net
 open System.Net.Http
 open Microsoft.WindowsAzure.Storage.Table
+open Newtonsoft.Json
 
 type Quark() =
     inherit TableEntity()
@@ -17,6 +18,13 @@ let XRun(req: HttpRequestMessage, inTable: IQueryable<Quark>, log: TraceWriter) 
     req.CreateResponse(HttpStatusCode.OK, "{message:\"Hello\"}")
 
 let Run(req: HttpRequestMessage, inTable: IQueryable<Quark>, log: TraceWriter) =
+    let sessions = query {
+            for quark in inTable do
+            select quark
+        }
+        |> JsonConvert.SerializeObject
+    req.CreateResponse(HttpStatusCode.OK, sessions)
+    (*
     let people =
         query {
             for person in inTable do
@@ -26,3 +34,4 @@ let Run(req: HttpRequestMessage, inTable: IQueryable<Quark>, log: TraceWriter) =
         |> String.concat ","
 
     req.CreateResponse(HttpStatusCode.OK, sprintf "{%s}" people)
+    *)
